42e8642528e732a50d15a4887c7d25870624a74d
Michelle Vanwettere <michellevanwettere@msn.com>
2020-11-24 16:28:19 -0500

    Add project files.

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Term_Project_Version_1.Models;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Term_Project_Version_1.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly SeekingAllahContext _context;
+
+        public MemberController(SeekingAllahContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Member
+        [Authorize(Roles = "Administrator,Manager,User")]
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["CountrySortParm"] = sortOrder == "Country" ? "country_desc" : "Country";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var members = from m in _context.Membership
+                          select m;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                members = members.Where(m => m.name.Contains(searchString)
+                || m.email.Contains(searchString)
+                || m.email.Contains(searchString)
+                || m.City.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    members = members.OrderByDescending(m => m.name);
+                    break;
+                case "Country":
+                    members = members.OrderByDescending(m => m.Country);
+                    break;
+                default:
+                    members = members.OrderBy(m => m.name);
+                    break;
+            }
+            int pageSize = 4;
+            return View(await PaginatedList<Members>.CreateAsync(members.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+        // GET: Member/Details/5
+        [Authorize(Roles = "Administrator,Manager, User")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var members = await _context.Membership
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (members == null)
+            {
+                return NotFound();
+            }
+
+            return View(members);
+        }
+
+        // GET: Member/Create
+        [Authorize(Roles = "Administrator,Manager")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Member/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,name,email")] Members members)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(members);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(members);
+        }
+
+        // GET: Member/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var members = await _context.Membership.FindAsync(id);
+            if (members == null)
+            {
+                return NotFound();
+            }
+            return View(members);
+        }
+
+        // POST: Member/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator,Manager")]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,name,email")] Members members)
+        {
+            if (id != members.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(members);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MembersExists(members.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(members);
+        }
+
+        // GET: Member/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var members = await _context.Membership
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (members == null)
+            {
+                return NotFound();
+            }
+
+            return View(members);
+        }
+
+        // POST: Member/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator,Manager")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var members = await _context.Membership.FindAsync(id);
+            _context.Membership.Remove(members);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MembersExists(int id)
+        {
+            return _context.Membership.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Migrations/20201113175302_Initial.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Term_Project_Version_1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Membership",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(maxLength: 40, nullable: false),
+                    email = table.Column<string>(maxLength: 50, nullable: false),
+                    Address = table.Column<string>(maxLength: 50, nullable: true),
+                    City = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true),
+                    Zipcode = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: true),
+                    ConfirmPassword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Membership", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Purchases",
+                columns: table => new
+                {
+                    ID = table.Column<string>(nullable: false),
+                    Price = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    MembersID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchases", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Purchases_Membership_MembersID",
+                        column: x => x.MembersID,
+                        principalTable: "Membership",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Membership",
+                columns: new[] { "ID", "Address", "City", "ConfirmPassword", "Country", "Password", "Zipcode", "State", "email", "name" },
+                values: new object[,]
+                {
+                    { 1, "Pastoor Celensstraat 1", "Kalmthout", null, "Belgium", null, "2920", null, "michelle@vanwetteregroup.com", "Michelle Vanwettere" },
+                    { 2, "Mohammed Mohammed Amr Allah", "Alexandria", null, "Egypt", null, "21624", null, "tito.nugaily@gmail.com", "Tito Nugauily" },
+                    { 3, null, "New York City", null, "United States", null, "10001", null, "david.wood@acts17.com", "David Wood" },
+                    { 4, "4138 Huntington Drive", "Traverse City", null, "United States", null, "49686", null, "mgibson@makeitrain.com", "Matthew Gibson" },
+                    { 5, "4131 Benedict Canyon Dr", "Los Angeles", null, "United States", null, "91423", null, "mohamedayew@gmail.com", "Mohamed Ayew" },
+                    { 6, "1219 Tetreau Street", "Thibodaux", null, "United States", null, "70301", null, "mayadamohamed@gmail.com", "Meda Doyle" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Purchases",
+                columns: new[] { "ID", "Description", "MembersID", "Price" },
+                values: new object[] { "SA", "Seeking Allah Finding Jesus Paperback", 1, "$19.99" });
+
+            migrationBuilder.InsertData(
+                table: "Purchases",
+                columns: new[] { "ID", "Description", "MembersID", "Price" },
+                values: new object[] { "FA", "Full Access including study guide and access to all podcasts and articles", 5, "$109.99" });
+
+            migrationBuilder.InsertData(
+                table: "Purchases",
+                columns: new[] { "ID", "Description", "MembersID", "Price" },
+                values: new object[] { "SG", "Study Guide including hard cover book, study guide and blu-ray", 6, "$79.99" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_MembersID",
+                table: "Purchases",
+                column: "MembersID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Purchases");
+
+            migrationBuilder.DropTable(
+                name: "Membership");
+        }
+    }
+}
